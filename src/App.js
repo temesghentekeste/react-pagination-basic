ba0c2b24bdef516d0ffd13bf38b9362e9cc8d47a
Temesghen <mail.temesghen.tekeste@gmail.com>
2021-06-30 10:52:40 +0300

    Refactor code; slice users & show first 10 users

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import "./App.css";
-import React, { useState } from "react";
-import JsonData from "./MOCK_DATA.json";
+import './App.css';
+import React, { useState } from 'react';
+import ReactPaginate from 'react-paginate';
+import JsonData from './MOCK_DATA.json';
 
 function App() {
   const [users, setUsers] = useState(JsonData.slice(0, 50));
+  const [pageNumber, setPageNumber] = useState(0);
 
-  return(
-   <div className="App">
-    {
-      users.map((user, index) => <div className="user">
+  const usersPerPage = 10;
+  const pagesVisited = pageNumber * usersPerPage;
+
+  const displayUsers = users
+    .slice(pagesVisited, pagesVisited + usersPerPage)
+    .map((user) => (
+      <div className="user">
         <h3>{user.firstName}</h3>
         <h3>{user.lastName}</h3>
         <h3>{user.email}</h3>
-      </div>)
-    }
-   </div>
-  )
+      </div>
+    ));
+
+  return <div className="App">{displayUsers}</div>;
 }
 
 export default App;
